7eb12fa4b59f40e45722535b61c188c15cf7ce4f
Harry <thinhtdse182756@fpt.edu.vn>
2024-06-17 13:45:30 +0700

    Home user

--- sourcecode/src/main/java/Controllers/User/ContactController.java
+++ sourcecode/src/main/java/Controllers/User/ContactController.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
-package Controller.user;
+package Controllers.User;
 
 import Utils.Email;
 import Utils.TemplateEmail;
